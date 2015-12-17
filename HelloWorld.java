fca046aefde4b705da4c721106cb0b8839c5b1d8
cairuhong <Ruhong.Cai@smartbear.com>
2015-12-17 16:46:55 -0600

    other_branch second modification
    modify to "otherbranch_22222222222"

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("otherbranch_3333333333333333Hello World!");
+		System.out.println("otherbranch_44444444444444444Hello World!");
 	}
 }
